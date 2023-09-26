bd2d3e0084e0f8ba67ff96478c7ec23caa7519eb
armanhabib353 <armanhabib353@gmail.com>
2023-09-27 02:47:58 +0600

    setup.py ready

---
+++ setup.py
@@ -0,0 +1,30 @@
+from typing import List
+from setuptools import find_packages, setup
+
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    # this function will return the list of requirements
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="dsProject",
+    version="1.0",
+    author="Arman Habib",
+    author_email="armanhabib353@gmail.com",
+    packages=find_packages(),
+    #install_require=['numpy', 'pandas']
+    install_require=get_requirements('requirements.txt')
+
+)
